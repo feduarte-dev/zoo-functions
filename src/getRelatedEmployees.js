c3321231287b58f58ee62d64497164eab0cd8ffb
Felipe Duarte <feduarte.dev@gmail.com>
2023-06-13 18:38:14 -0300

    quarto requisito pronto

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) =>
+  data.employees.some((employee) => employee.managers.includes(id));
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees
+      .filter((employee) => employee.managers.includes(managerId))
+      .map((person) => `${person.firstName} ${person.lastName}`);
+  } else {
+    throw  new  Error(`O id inserido não é de uma pessoa colaboradora gerente!`);
+  }
 };
 
+console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
