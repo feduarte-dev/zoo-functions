c502f7d285c1dd706ef0c80d17ec46382dc1d025
Felipe Duarte <feduarte.dev@gmail.com>
2023-06-14 19:44:34 -0300

    3/7 requisitos do ultimo exercicio feitos

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
-const getAnimalMap = (options) => {
-  // seu código aqui
-};
+const noOption = () =>
+  ({
+    NE: data.species
+      .filter((specie) => specie.location === 'NE')
+      .map((animal) => animal.name),
+    NW: data.species
+      .filter((specie) => specie.location === 'NW')
+      .map((animal) => animal.name),
+    SE: data.species
+      .filter((specie) => specie.location === 'SE')
+      .map((animal) => animal.name),
+    SW: data.species
+      .filter((specie) => specie.location === 'SW')
+      .map((animal) => animal.name),
+  });
 
+const getAnimalMap = (options) => {
+  if (!options || options.includeNames === false || options.sorted === false || options.sex) {
+    return noOption();
+  }
+};
+console.log(getAnimalMap({ sex: 'female', sorted: true }));
 module.exports = getAnimalMap;
