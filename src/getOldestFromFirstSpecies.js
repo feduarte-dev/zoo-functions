f9a8ff386a49a841520d4f89ca9996f6639c20e9
Felipe Duarte <feduarte.dev@gmail.com>
2023-06-14 13:42:44 -0300

    decimo requisito completo

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
-};
+const findEmployee = (id) =>
+  data.employees.find((employee) => employee.id === id).responsibleFor[0];
 
+const getOldestFromFirstSpecies = (id) =>
+  Object.values(
+    data.species
+      .find((specie) => specie.id === findEmployee(id))
+      .residents.reduce((bigger, number) =>
+        (bigger.age > number.age ? bigger : number)),
+  );
+
+console.log(getOldestFromFirstSpecies('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 module.exports = getOldestFromFirstSpecies;
