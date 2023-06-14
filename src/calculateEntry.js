c10ac4518ef6f32f3081cb01ee2d1a85d7259d14
Felipe Duarte <feduarte.dev@gmail.com>
2023-06-14 11:36:27 -0300

    requisito 8 completo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui
-};
+const countEntrants = (entrants) => ({
+  adult: entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50)
+    .length,
+  child: entrants.filter((entrant) => entrant.age < 18).length,
+  senior: entrants.filter((entrant) => entrant.age >= 50).length,
+});
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
+  const { adult, child, senior } = countEntrants(entrants);
+  const result = adult * 49.99 + child * 20.99 + senior * 24.99;
+  return result;
 };
 
 module.exports = { calculateEntry, countEntrants };
