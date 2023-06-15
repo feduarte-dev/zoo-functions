13196dfbad69a433171150a0550094488f8adf7a
Felipe Duarte <feduarte.dev@gmail.com>
2023-06-15 14:57:07 -0300

    gg

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,89 +1,88 @@
-// const data = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
-// const map = { NE: [], NW: [], SE: [], SW: [] };
+const noOption = () => {
+  const map = { NE: [], NW: [], SE: [], SW: [] };
+  Object.keys(map).forEach((location) =>
+    data.species
+      .filter((specie) => specie.location === location)
+      .map((animal) => map[location].push(animal.name)));
+  return map;
+};
 
-// const noOption = () => {
-//   Object.keys(map).forEach((location) =>
-//     data.species
-//       .filter((specie) => specie.location === location)
-//       .map((animal) => map[location].push(animal.name)));
-//   return map;
-// };
+const includeNamesFunction = () => {
+  const map = { NE: [], NW: [], SE: [], SW: [] };
+  Object.keys(map).forEach((location) =>
+    data.species
+      .filter((specie) => specie.location === location)
+      .map((animal) =>
+        map[location].push({
+          [animal.name]: animal.residents.map(
+            (animalNames) => animalNames.name,
+          ),
+        })));
+  return map;
+};
 
-// const includeNamesFunction = () => {
-//   Object.keys(map).forEach((location) =>
-//     data.species
-//       .filter((specie) => specie.location === location)
-//       .map((animal) =>
-//         map[location].push({
-//           [animal.name]: animal.residents.map(
-//             (animalNames) => animalNames.name,
-//           ),
-//         })));
-//   return map;
-// };
+const sortNames = () => {
+  const map = { NE: [], NW: [], SE: [], SW: [] };
+  Object.keys(map).forEach((location) =>
+    data.species
+      .filter((specie) => specie.location === location)
+      .map((animal) =>
+        map[location].push({
+          [animal.name]: animal.residents
+            .map((animalNames) => animalNames.name)
+            .sort(),
+        })));
+  return map;
+};
 
-// const sortNames = () => {
-//   Object.keys(map).forEach((location) =>
-//     data.species
-//       .filter((specie) => specie.location === location)
-//       .map((animal) =>
-//         map[location].push({
-//           [animal.name]: animal.residents
-//             .map((animalNames) => animalNames.name)
-//             .sort(),
-//         })));
-//   return map;
-// };
+const filterGender = (gender) => {
+  const map = { NE: [], NW: [], SE: [], SW: [] };
+  Object.keys(map).forEach((location) =>
+    data.species
+      .filter((specie) => specie.location === location)
+      .map((animal) =>
+        map[location].push({
+          [animal.name]: animal.residents
+            .filter((animalGender) => animalGender.sex === gender)
+            .map((animalNames) => animalNames.name),
+        })));
+  return map;
+};
 
-// const filterGender = (gender) => {
-//   Object.keys(map).forEach((location) =>
-//     data.species
-//       .filter((specie) => specie.location === location)
-//       .map((animal) =>
-//         map[location].push({
-//           [animal.name]: animal.residents
-//             .filter((animalGender) => animalGender.sex === gender)
-//             .map((animalNames) => animalNames.name),
-//         })));
-//   return map;
-// };
+const filteredAndSorted = (gender) => {
+  const map = { NE: [], NW: [], SE: [], SW: [] };
+  Object.keys(map).forEach((location) =>
+    data.species
+      .filter((specie) => specie.location === location)
+      .map((animal) =>
+        map[location].push({
+          [animal.name]: animal.residents
+            .filter((animalGender) => animalGender.sex === gender)
+            .map((animalNames) => animalNames.name).sort(),
+        })));
+  return map;
+};
 
-// const filteredAndSorted = (gender) => {
-//   Object.keys(map).forEach((location) =>
-//     data.species
-//       .filter((specie) => specie.location === location)
-//       .map((animal) =>
-//         map[location].push({
-//           [animal.name]: animal.residents
-//             .filter((animalGender) => animalGender.sex === gender)
-//             .map((animalNames) => animalNames.name).sort(),
-//         })));
-//   return map;
-// };
+const sort = (options) => {
+  if (options.sorted && !options.sex) {
+    return sortNames();
+  }
+  if (options.sorted && options.sex) {
+    return filteredAndSorted(options.sex);
+  }
+};
 
-// const getAnimalMap = (options) => {
-//   if (!options) {
-//     return noOption();
-//   }
-//   if (!options.includeNames && options.sex && options.sorted) {
-//     return noOption();
-//   }
-//   if (!options.includeNames && options.sex) {
-//     return noOption();
-//   }
-//   if (options.includeNames && !options.sorted && !options.sex) {
-//     return includeNamesFunction();
-//   }
-//   if (options.includeNames && options.sorted && !options.sex) {
-//     return sortNames();
-//   }
-//   if (options.includeNames && options.sex && !options.sorted) {
-//     return filterGender(options.sex);
-//   }
-//   if (options.includeNames && options.sorted && options.sex) {
-//     return filteredAndSorted(options.sex);
-//   }
-// };
-// // console.log(getAnimalMap({ includeNames: true, sex: 'male', sorted: true }).NE);
-// module.exports = getAnimalMap;
+const noSort = (options) => {
+  if (!options.sorted && !options.sex) { return includeNamesFunction(); }
+  if (!options.sorted && options.sex) { return filterGender(options.sex); }
+  return sort(options);
+};
+
+const getAnimalMap = (options) => {
+  if (options === undefined || !options.includeNames) { return noOption(); }
+  return noSort(options);
+};
+
+module.exports = getAnimalMap;
