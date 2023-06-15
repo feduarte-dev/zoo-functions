3621bef455d7a4f9fcfb289482a43854944f5e66
Felipe Duarte <feduarte.dev@gmail.com>
2023-06-15 13:34:48 -0300

    tentando requisito 14 x2

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,33 +1,53 @@
-const data = require('../data/zoo_data');
+// const data = require('../data/zoo_data');
 
-const map = { NE: [], NW: [], SE: [], SW: [] };
+// const map = { NE: [], NW: [], SE: [], SW: [] };
 
-const noOption = () => {
-  Object.keys(map).forEach((location) =>
-    data.species
-      .filter((specie) => specie.location === location)
-      .map((animal) => map[location].push(animal.name)));
-  return map;
-};
+// const noOption = () => {
+//   Object.keys(map).forEach((location) =>
+//     data.species
+//       .filter((specie) => specie.location === location)
+//       .map((animal) => map[location].push(animal.name)));
+//   return map;
+// };
 
-const includeNamesFunction = () => {
-  Object.keys(map).forEach((location) =>
-    data.species
-      .filter((specie) => specie.location === location)
-      .map((animal) => map[location].push({
-        [animal.name]: animal.residents.map(
-          (animalNames) => animalNames.name,
-        ),
-      }))); return map;
-};
+// const includeNamesFunction = () => {
+//   Object.keys(map).forEach((location) =>
+//     data.species
+//       .filter((specie) => specie.location === location)
+//       .map((animal) => map[location].push({
+//         [animal.name]: animal.residents.map(
+//           (animalNames) => animalNames.name,
+//         ),
+//       }))); return map;
+// };
 
-const getAnimalMap = (options) => {
-  if (!options) {
-    return noOption();
-  }
-  if (options.includeNames === true) {
-    return includeNamesFunction();
-  }
-};
-console.log(getAnimalMap({ includeNames: true }));
-module.exports = getAnimalMap;
+// const sortNames = () => {
+//   Object.keys(map).forEach((location) =>
+//     data.species
+//       .filter((specie) => specie.location === location)
+//       .map((animal) => map[location].push({
+//         [animal.name]: animal.residents.map(
+//           (animalNames) => animalNames.name,
+//         ).sort(),
+//       }))); return map;
+// };
+
+// const getAnimalMap = (options) => {
+//   if (!options) {
+//     return noOption();
+//   }
+//   if (!options.includeNames && options.sex && options.sorted) {
+//     return noOption();
+//   }
+//   if (!options.includeNames && options.sex) {
+//     return noOption();
+//   }
+//   if (options.includeNames === true && !options.sorted) {
+//     return includeNamesFunction();
+//   }
+//   if (options.includeNames === true && options.sorted) {
+//     return sortNames();
+//   }
+// };
+// // console.log(getAnimalMap({ includeNames: true, sorted: true }).NE);
+// module.exports = getAnimalMap;
